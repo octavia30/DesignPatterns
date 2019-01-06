881b45c99ce4ba2de1bf255207c39a317372d349
octavia30 <37707065+octavia30@users.noreply.github.com>
2019-01-07 00:22:22 +0200

    Add files via upload

---
+++ DesignPatterns/src/patterns/command/StatisticsCommand.java
@@ -0,0 +1,16 @@
+
+package patterns.command;
+
+import patterns.model.BookStatistics;
+import patterns.singleton.DocumentManager;
+
+public class StatisticsCommand implements Command {
+
+  @Override
+  public void execute() {
+    BookStatistics stats = new BookStatistics();
+    DocumentManager.getInstance().getBook().accept(stats);
+    stats.printStatistics();
+  }
+
+}
---
+++ DesignPatterns/src/patterns/model/Author.java
@@ -0,0 +1,14 @@
+
+package patterns.model;
+
+public class Author {
+  private String name;
+
+  public Author(String name) {
+    this.name = name;
+  }
+
+  public void print() {
+    System.out.println("Author: " + name);
+  }
+}
